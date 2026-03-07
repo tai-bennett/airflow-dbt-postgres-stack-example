9b177a17a27803662ff69be3db6bea3e50777f13
db-archy <bennettduncantai@gmail.com>
2026-03-06 19:43:24 -0700

    update example

--- airflow/dags/test.py
+++ airflow/dags/test.py
@@ -3,8 +3,10 @@ Hello, this is a test.
 """
 
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.providers.standard.operators.bash import BashOperator
 from datetime import datetime
+from airflow.sdk import task, dag
+import pendulum
 
 # [START default args]
 default_args = {
@@ -17,36 +19,51 @@ default_args = {
 }
 # [END default args]
 
-# [START instantiate dags]
-init_dag = DAG(
-    "1_init_dag",
-    default_args=default_args,
-    schedule_interval=@once
-)
-test_dag = DAG(
-    "2_test_dag",
-    default_args=default_args,
-    schedule_interval=None
-)
-# [END instantiate dags]
+# THIS IS THE OLD METHOD
+# # [START instantiate dags]
+# with DAG("1_init_dag",default_args=default_args) as dag:
+#     t1 = BashOperator(
+#         task_id="touch_file",
+#         bash_command="touch output.txt"
+#     )
+# 
+#     t2 = BashOperator(
+#         task_id="date",
+#         bash_command="date >> ./output.txt"
+#     )
+# 
+#     t3 = BashOperator(
+#         task_id="whoami",
+#         bash_command="whoami"
+#     )
+# 
+#     # set the dependency of these tasks t1, t2, t3
+#     t1 >> t2 >> t3
+# # [END instantiate dags]
 
-t1 = BashOperator(
-    task_id="touch file",
-    bash_command="touch ./output.txt",
-    dag=init_dag
-)
+# This is the pythonic style dags
 
-t2 = BashOperator(
-    task_id="date",
-    bash_command="date >> ./output.txt",
-    dag=test_dag
+@dag(
+    schedule=None,
+    start_date=pendulum.datetime(2020, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["test"]
 )
+def test_pipeline():
+    @task(multiple_outputs=True)
+    def step1():
+        return {"a": 100, "b": 200, "c": 300}
+    @task
+    def step2(input: dict):
+        out = 0
+        for v in input.values():
+            out += v
+        return out
+    @task
+    def step3(input):
+        print("The total is: " + str(input))
+    out = step1()
+    out = step2(out)
+    step3(out)
 
-t3 = BashOperator(
-    task_id="whoami",
-    bash_command="whoami >> ./output.txt",
-    dag=test_dag
-)
-
-t1
-t2 >> t3
+test_pipeline()
